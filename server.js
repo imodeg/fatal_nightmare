972bfb7020cb35aa2d1e26ee87c7d040aafad22d
i-mod <imodeg@gmail.com>
2017-12-03 14:06:00 +0500

    ser

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
 
 app.use(require('express').static(__dirname + '/'));
-
+// [START hello_world]
+// Say hello!
 app.get('/', function (req, res)
 {
   //res.sendFile('index.html');
   res.sendFile('index.html', {root: __dirname});
 });
+// [END hello_world]
 
-app.listen(3000, function ()
-{
-  console.log('Start server!');
-});
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
